fe568b16124feed3881e4a9ecd7492e0dc226690
Dohyeon_Parrk <parkyj3213@naver.com>
2024-11-12 17:34:24 +0900

    feat : 폴더 별 관심상품 조회 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -1,7 +1,5 @@
 package com.sparta.myselectshop.repository;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +9,5 @@ import com.sparta.myselectshop.entity.User;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	Page<Product> findAllByUser(User user, Pageable pageable);
+	Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
 }
